8eb4405d8d64ffeb66acd5036a56f787ba5ab79f
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-02-25 17:57:29 -0500

    Cors error issue fix

--- Back-End/src/main/java/com/example/recipeapp/controller/RecipeController.java
+++ Back-End/src/main/java/com/example/recipeapp/controller/RecipeController.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.recipeapp.persistence.RecipeOps;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/")
 public class RecipeController {
