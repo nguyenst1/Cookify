72e17e9c4b8dc2b0741153f0b850051f960eda28
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-02-26 09:56:22 -0500

    Backend login and authentication done

--- Back-End/src/main/java/com/example/recipeapp/controller/RecipeController.java
+++ Back-End/src/main/java/com/example/recipeapp/controller/RecipeController.java
@@ -1,31 +1,32 @@
 package com.example.recipeapp.controller;
 
-import java.util.HashMap;
-import java.util.Optional;
-import java.util.logging.Logger;
-
 import org.json.JSONObject;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.recipeapp.persistence.RecipeOps;
+import com.example.recipeapp.persistence.UserOps;
 
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/")
 public class RecipeController {
-    private static final Logger LOG = Logger.getLogger(RecipeController.class.getName());
     private RecipeOps recipeOps;
+    private UserOps userOps;
 
-    public RecipeController(RecipeOps recipeOps) {
+    public RecipeController(RecipeOps recipeOps, UserOps userOps) {
         this.recipeOps = recipeOps;
+        this.userOps = userOps;
     }
 
     @GetMapping("/recipe")
@@ -46,5 +47,37 @@ public class RecipeController {
     public ResponseEntity<String> getRecipeImage(@RequestParam("food_instruction") String foodInstruction)
             throws Exception {
         return new ResponseEntity<>(recipeOps.getImage(foodInstruction).toString(), HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody JSONObject credentials) throws Exception{
+        if(!(credentials.has("email") && credentials.has("password")) && (credentials.has("username"))){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        
+        JSONObject userData = userOps.registerUser(credentials.getString("email"), credentials.getString("username"), credentials.getString("password"));
+        return new ResponseEntity<>(userData.toString(), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticateUser(@RequestBody JSONObject credentials){
+        if(!credentials.has("password") && !(credentials.has("username") || credentials.has("email"))){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        
+        Boolean isAuthenticated = false;
+        String userId = "";
+        if(credentials.has("username")){
+            isAuthenticated = userOps.authenticateUser(credentials.getString("username"), credentials.getString("password"), false);
+            if(isAuthenticated){
+                userId = userOps.getUserId(credentials.getString("username"), false);
+            }
+        }else  if(credentials.has("email")){
+            isAuthenticated = userOps.authenticateUser(credentials.getString("email"), credentials.getString("password"), true);
+            if(isAuthenticated){
+                userId = userOps.getUserId(credentials.getString("email"), true);
+            }
+        }
+        return isAuthenticated ? new ResponseEntity<>(new JSONObject().put("user_id", userId).toString() ,HttpStatus.OK) : new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 }
--- Back-End/src/main/java/com/example/recipeapp/persistence/UserOps.java
+++ Back-End/src/main/java/com/example/recipeapp/persistence/UserOps.java
@@ -1,19 +1,47 @@
 package com.example.recipeapp.persistence;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.example.recipeapp.db_utils.UserUtils;
+
 public class UserOps {
-    public JSONObject authenticateUser(String userName, String password) {
-        return null;
+    private UserUtils userUtils;
+
+    public UserOps(UserUtils userUtils){
+        this.userUtils = userUtils;
     }
-    public JSONObject registerUser(String email, String password) {
-        if(authenticateUser(email, password) != null){
-            return null;
+    public JSONObject getUser(String data, Boolean isEmail){
+        JSONArray userData = new JSONArray();
+        if(isEmail){
+            userData = userUtils.getUserByConditions(data, null, null);
+        }else{
+            userData = userUtils.getUserByConditions(null, data, null);
         }
-        // UserUtils.insertUserData(userName, password, type);
-        // User user = UserUtils.getUserDetails(userName, password);
-        // new NotificationsOps().updateNotificationOnUserRegister(user.getId());
-        // return user;
+        return !userData.isEmpty() ? userData.getJSONObject(0) : new JSONObject();
+    }
+    public String getUserId(String data, Boolean isEmail){
+        JSONArray userData = new JSONArray();
+        if(isEmail){
+            userData = userUtils.getUserByConditions(data, null, null);
+        }else{
+            userData = userUtils.getUserByConditions(null, data, null);
+        }
+        return !userData.isEmpty() ? userData.getJSONObject(0).getString("id") : null;
+    }
+    public Boolean authenticateUser(String data, String password, Boolean isEmail) {
+        return !isEmail ? !userUtils.getUserByConditions(null, data, password).isEmpty() : !userUtils.getUserByConditions(data, null, password).isEmpty();
+    }
+    public JSONObject registerUser(String email, String userName, String password) throws Exception {
+        if(authenticateUser(email, password, true)){
+            if(userUtils.createUser(email, userName, password)){
+                JSONArray userData = userUtils.getUserByConditions(email, userName, password);
+                if(!userData.isEmpty()){
+                    return userData.getJSONObject(0);
+                }
+            }
+
+        }
         return null;
     }
 }
